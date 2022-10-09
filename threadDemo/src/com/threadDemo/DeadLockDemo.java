55c008518b77443a0ce2fe52f0e9a4f989ab62c1
lihaohan <plumsun@163.com>
2022-10-09 17:09:56 +0800

    优化

--- threadDemo/src/com/threadDemo/DeadLockDemo.java
+++ threadDemo/src/com/threadDemo/DeadLockDemo.java
@@ -1,5 +1,8 @@
 package com.threadDemo;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * @description: 死锁演示与排查
  * @date: 2022/5/27 11:39
@@ -10,5 +13,17 @@ public class DeadLockDemo {
 
     public static void main(String[] args) {
 
+
+    }
+    private static void extracted() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        if(list.size() == 1){
+            if(!list.get(0).contains("1")){
+                System.out.println(" = 1");
+            }
+        }else if(list.size() != 0){
+            System.out.println("= 0");
+        }
     }
 }
